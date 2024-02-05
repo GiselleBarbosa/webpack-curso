3494a00aeecac0f40b7064177db8cfca4e0b31b3
GiselleBarbosa <g88barbosa@gmail.com>
2024-02-05 14:08:00 -0300

    integrado bootstrap

--- 5_integracoes/src/index.js
+++ 5_integracoes/src/index.js
@@ -1,5 +1,6 @@
 // import fonts
 import "./fonts.css";
+import "./index.scss";
 
 // import jquery
 import $ from "jquery";
@@ -8,11 +9,15 @@ import $ from "jquery";
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faBacon, faAnchor } from "@fortawesome/free-solid-svg-icons";
 
+// import bootstrap
+import "bootstrap";
+
 library.add(faBacon);
 library.add(faAnchor);
 dom.watch();
 
 const body = $("body");
+
 const p = $("<p></p>").text("inserindo texto").css("color", "red");
 
 body.append(p);
--- 5_integracoes/webpack.config.js
+++ 5_integracoes/webpack.config.js
@@ -6,12 +6,17 @@ module.exports = {
 		filename: "bundle.js",
 		path: path.resolve(__dirname, "./dist"),
 	},
+	mode: "development",
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
 				exclude: /node_modules/,
 				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
 			},
 			{
 				test: /\.(ttf|woff|woff2)$/,
