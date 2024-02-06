5bc86e5b8eb5fba46733126371a813a5106224e4
GiselleBarbosa <g88barbosa@gmail.com>
2024-02-06 18:27:57 -0300

    adicionado o "include" para o build somente de diretorios especificos e configurado servidor com hot reload

--- 11_recursos_avancados_webpack/webpack.config.js
+++ 11_recursos_avancados_webpack/webpack.config.js
@@ -7,6 +7,21 @@ module.exports = {
 		path: path.resolve(__dirname, "./dist"),
 		filename: "bundle.js",
 	},
+
+	devServer: {
+		historyApiFallback: true,
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: "babel-loader",
+				include: path.resolve(__dirname, "src"),
+			},
+		],
+	},
+
 	mode: "production",
 	plugins: [
 		new webpack.ProvidePlugin({
