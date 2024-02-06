3167bdcbfb42e2d0d0fb3b296e935ad87eb4003d
GiselleBarbosa <g88barbosa@gmail.com>
2024-02-06 19:17:43 -0300

    removidos comentarios e removido um exemplo repetido.

--- 11_recursos_avancados_webpack/src/index.js
+++ 11_recursos_avancados_webpack/src/index.js
@@ -1,16 +1,8 @@
 import print from "./msg.js";
 
 console.log("Primeiro teste com lodash", _.difference([2, 1], [2, 3]));
-console.log("Segundo teste com lodash", _.difference([1, 2, 3], [1, 4, 5]));
 
-console.log(
-	"Terceiro teste com lodash apos criacao de variavel global apenas nas config do webpack",
-	_.lastIndexOf([1, 2, 3], 2)
-);
-
-// Some libraries depend on global variables. For example, a jQuery plugin may expect the "$" global variable to be available.
-
-// Without shimming that variable with ProvidePlugin you would get an error $ is not defined.
+console.log(_.lastIndexOf([1, 2, 3], 2));
 
 if (module.hot) {
 	module.hot.accept("./msg.js", function () {
