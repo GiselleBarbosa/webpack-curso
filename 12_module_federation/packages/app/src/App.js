00ac5fa1425ef1f0c6c4935a95cb6bf601ad8f74
GiselleBarbosa <g88barbosa@gmail.com>
2024-02-07 15:19:44 -0300

    ✅ finalizada a integracao dos MFEs

--- 12_module_federation/packages/app/src/App.js
+++ 12_module_federation/packages/app/src/App.js
@@ -5,6 +5,10 @@ import { Navbar, Nav, NavItem } from "reactstrap";
 import "./app.css";
 
 const App = () => {
+	// Estas constantes serão os componentes que serão chamados por demanda
+	// Utilizando o SWITCH do React é possivel intercalar um "loading" e utilizando o Suspense* e com o "exact path" é carregado o componente selecionado.
+	const HomePage = React.lazy(() => import("HomeApp/HomePage"));
+	const ContactPage = React.lazy(() => import("ContactApp/ContactPage"));
 
 	return (
 		<Router>
@@ -19,6 +23,19 @@ const App = () => {
 						</NavItem>
 					</Nav>
 				</Navbar>
+
+				<Switch>
+					<Route exact path="/">
+						<Suspense fallback={<div>Carregando a página solicitada...</div>}>
+							<HomePage />
+						</Suspense>
+					</Route>
+					<Route exact path="/contact">
+						<Suspense fallback={<div>Carregando a página solicitada...</div>}>
+							<ContactPage />
+						</Suspense>
+					</Route>
+				</Switch>
 			</div>
 		</Router>
 	);
